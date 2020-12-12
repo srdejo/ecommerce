fd2b0cfaf1a8c79f634f5f0a3e0ed544610ae160
srdejo_wposs <danielovallos@wposs.com>
2020-12-12 12:11:54 -0500

    Estructura del proyecto

---
+++ register.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.79.0">
+    <title>Ecommerce - WPOSS</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    
+<main class="form-signin">
+  <form>
+    <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Ecommerce - WPOSS</h1>
+    <label for="inputEmail" class="visually-hidden">Usuario</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+    <label for="inputPassword" class="visually-hidden">Asignar Contraseña</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+    <label for="inputPassword" class="visually-hidden">Repetir Contraseña</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+    <select class="form-select" aria-label="Default select example">
+        <option selected>Seleccione un rol</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
+    
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Registrar</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+  </form>
+</main>
+
+
+    
+  </body>
+</html>
